d9c9466ebc5c22629f4a92e45f69499cf5bdf32a
hosam mohamed <hosamelmalt@gmail.com>
2020-11-26 07:34:25 -0800

    Initial Commit

---
+++ Recipes Swiftui/Recipes_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_SwiftuiApp.swift
+//  Recipes Swiftui
+//
+//  Created by Hossam on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
